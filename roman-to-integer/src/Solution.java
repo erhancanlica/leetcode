b2112d32d92b7f48bcebde3c9815f933489abbc1
Erhan <erhancnlc@outlook.com>
2022-01-18 13:31:19 +0300

    romant o integer solved

--- roman-to-integer/src/Solution.java
+++ roman-to-integer/src/Solution.java
@@ -1,68 +1,30 @@
+import java.util.HashMap;
+
 public class Solution {
     public static void main(String[] args) {
-        int result = romanToInt("LVIII");
+        int result = romanToInt("MCMXCIV");
         System.out.println(result);
     }
 
     public static int romanToInt(String s) {
         int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case ('I'):
-                    if (s.charAt(i + 1) == 'V') {
-                        sum += 4;
-                        i++;
-                        break;
-                    } else if (s.charAt(i + 1) == 'X') {
-                        sum += 9;
-                        i++;
-                        break;
-                    } else {
-                        sum += 1;
-                        break;
-                    }
-                case ('V'):
-                    sum += 5;
-                    break;
-                case ('X'):
-                    if (s.charAt(i + 1) == 'L') {
-                        sum += 40;
-                        i++;
-                        break;
-                    } else if (s.charAt(i + 1) == 'C') {
-                        sum += 90;
-                        i++;
-                        break;
-                    } else {
-                        sum += 10;
-                        break;
-                    }
-                case ('L'):
-                    sum += 50;
-                    break;
-                case ('C'):
-                    if (s.charAt(i + 1) == 'D') {
-                        sum += 400;
-                        i++;
-                        break;
-                    } else if (s.charAt(i + 1) == 'M') {
-                        sum += 900;
-                        i++;
-                        break;
-                    } else {
-                        sum += 100;
-                        break;
-                    }
-                case ('D'):
-                    sum += 500;
-                    break;
-                case ('M'):
-                    sum += 1000;
-                    break;
-                default:
-                    System.out.println("Geçerli bir sayı değil");
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                sum = sum - map.get(s.charAt(i));
+            } else {
+                sum = sum + map.get(s.charAt(i));
             }
         }
+
+        sum = sum + map.get(s.charAt(s.length() - 1));
         return sum;
     }
 }
