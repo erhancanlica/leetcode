215c1a1b95526038f70f1da49b22962b52732180
Erhan <erhancnlc@outlook.com>
2022-01-18 16:28:13 +0300

    valid parantheses algorithm is solved

--- longest-common-prefix/src/Solution.java
+++ longest-common-prefix/src/Solution.java
@@ -7,14 +7,28 @@ public class Solution {
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
 
+    private static String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String lcpLeft = longestCommonPrefix(strs, l, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    private static String commonPrefix(String lcpLeft, String lcpRight) {
+        int min = Math.min(lcpLeft.length(), lcpRight.length());
+        for (int i = 0; i < min; i++) {
+            if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                return lcpLeft.substring(0, i);
             }
         }
-        return prefix;
+        return lcpLeft.substring(0, min);
     }
 }
